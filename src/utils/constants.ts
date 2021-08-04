9aadcc5879aa62d26f4981854bfd5bc519ac9e58
Taeuk Gwak <gwak2837@kakao.com>
2021-08-05 01:01:59 +0900

    Minor change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,8 @@
 /** 백엔드 API 주소 */
-const API_URL = process.env.API_URL || 'http://localhost:8000'
+export const API_URL = process.env.API_URL || 'http://localhost:8000'
 
 /** 백엔드 API v1 주소 */
-const API_V1_URL = API_URL + '/v1'
+export const API_V1_URL = API_URL + '/v1'
 
 /** QR 코드 암호화를 위한 비밀 키 */
-const CRYPTO_SECRET_KEY = process.env.CRYPTO_SECRET_KEY || 'foo'
-
-export { API_URL, API_V1_URL, CRYPTO_SECRET_KEY }
+export const CRYPTO_SECRET_KEY = process.env.CRYPTO_SECRET_KEY || 'foo'
