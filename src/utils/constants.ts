d5fe41f52c1e736285c6427a143f942d32b56733
Taeuk Gwak <gwak2837@kakao.com>
2021-08-05 10:42:48 +0900

    Fix importing error?

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,10 @@
 /** 백엔드 API 주소 */
-export const API_URL = process.env.API_URL || 'http://localhost:8000'
+const API_URL = process.env.API_URL || 'http://localhost:8000'
 
 /** 백엔드 API v1 주소 */
-export const API_V1_URL = API_URL + '/v1'
+const API_V1_URL = API_URL + '/v1'
 
 /** QR 코드 암호화를 위한 비밀 키 */
-export const CRYPTO_SECRET_KEY = process.env.CRYPTO_SECRET_KEY || 'foo'
+const CRYPTO_SECRET_KEY = process.env.CRYPTO_SECRET_KEY || 'foo'
+
+export { API_URL, API_V1_URL, CRYPTO_SECRET_KEY }
