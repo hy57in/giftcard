e5f557e9f1d175f35b2ec0af518b9e71a307e1be
HyojinKim <gywls00100@gmail.com>
2021-08-08 16:19:58 +0900

    feat: add react-ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 import NavigationBar from 'src/components/NavigationBar/NavigationBar'
+import ReactGA from 'react-ga'
+
+ReactGA.initialize('282134203')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
